cb461cc17f48abd7c1d28c285d778c998ce8a49f
Ariel200609 <arielmontoya200609@gmail.com>
2025-10-03 10:14:01 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AmorEstelar/',
+  base: '/FieltroBonito/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
